70cc7b3c0f6910bc8b5998b20906cddc2b64a62e
Saravanan <saravananchandrasekaran@kuyatechnologies.com>
2020-03-06 12:11:00 +0530

    Get all possible numbers inside the given number

--- PalindromeFinder/PalindromeFinder/Program.cs
+++ PalindromeFinder/PalindromeFinder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace PalindromeFinder
 {
@@ -6,7 +8,37 @@ namespace PalindromeFinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                int Input = 12234325;
+
+                double totalLength = Math.Floor(Math.Log10(Input) + 1);
+                List<int> possibleNumbers = new List<int>();
+                char[] inputArray = Input.ToString().ToCharArray();
+
+                //selecting number of digits in incremental order
+                //using for loop, since it's faster than foreach
+                for (int i = 2; i <= totalLength; i++)
+                {
+                    for (int j = 0; j < totalLength; j++)
+                    {
+                        StringBuilder builder = new StringBuilder();
+                        for (int x = j; x < j + i; x++)
+                        {
+                            if (x < totalLength)
+                                builder.Append(inputArray[x]);
+                        }
+
+                        possibleNumbers.Add(Convert.ToInt32(builder.ToString()));
+                    }
+                }
+
+                possibleNumbers.ForEach(x => { Console.WriteLine(x); });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong. Message: " + ex.Message);
+            }
         }
     }
 }
