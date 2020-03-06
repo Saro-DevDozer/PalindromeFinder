f59c7d0b476f0dc341b4704a61bf1cdf2a2ff4ee
Saravanan <saravananchandrasekaran@kuyatechnologies.com>
2020-03-06 11:44:12 +0530

    Initial commit
    Created console application project in .net core

---
+++ PalindromeFinder/PalindromeFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalindromeFinder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
